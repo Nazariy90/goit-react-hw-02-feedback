bf06d27c87e9b1682c2f632f38c9b52bf7de0470
Nazariy90 <pinhvin123@gmail.com>
2023-04-26 21:37:27 +0300

    add task 2

--- src/components/feedback/feedback.jsx
+++ src/components/feedback/feedback.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import css from './feedback.module.css';
 
 class Feedback extends React.Component{
-
 state = {
   good: 0,
   neutral: 0,
   bad: 0
 }
-
     handleGood = () => {
         this.setState(prevState => ({
            good: prevState.good + 1,
@@ -24,6 +22,17 @@ state = {
            bad: prevState.bad + 1,
         }) )
     }
+
+    countTotalFeedback = () => {
+        const { good, neutral, bad } = this.state;
+        return good + neutral + bad;
+    }
+
+    countPositiveFeedbackPercentage = () => {
+        const { good, neutral, bad } = this.state;
+        const totalFeedback = good + neutral + bad;
+        return totalFeedback === 0 ? 0 : Math.round((good / totalFeedback) * 100);
+    }
     
     render() {
     return (
@@ -36,9 +45,11 @@ state = {
         </div>
     <h2 className={css.title}>Statistics</h2>    
             <ul className={css.statisticsList}>
-                <li className={css.statisticsItem}>Good: <span className={css.statisticsItemValue}>{this.state.good}</span></li>
-                <li className={css.statisticsItem}>Neutral: <span className={css.statisticsItemValue}>{this.state.neutral}</span></li>
-                <li className={css.statisticsItem}>Bad: <span className={css.statisticsItemValue}>{this.state.bad}</span></li>                       
+            <li className={css.statisticsItem}>Good: <span className={css.statisticsItemValue}>{this.state.good}</span></li>
+            <li className={css.statisticsItem}>Neutral: <span className={css.statisticsItemValue}>{this.state.neutral}</span></li>
+            <li className={css.statisticsItem}>Bad: <span className={css.statisticsItemValue}>{this.state.bad}</span></li>
+            <li className={css.statisticsItem}>Total: <span className={css.statisticsItemValue}>{this.countTotalFeedback()}</span></li>
+            <li className={css.statisticsItem}>Positive feedback: <span className={css.statisticsItemValue}>{this.countPositiveFeedbackPercentage()}%</span></li>
             </ul>    
 </div>
         )
